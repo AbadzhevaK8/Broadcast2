e0f800d621e0c48bd010bc578358823bd66d6c51
k8abadzheva <make.me.loco@gmail.com>
2024-12-25 22:51:09 +0300

    Add progress bar and receiver for broadcast
    Added a progress bar to the main activity and registered a broadcast receiver to update its progress.
    The receiver listens for the "loaded" action and updates the progress bar with the percentage value from the intent.

--- app/src/main/java/com/abadzheva/broadcast2/MainActivity.kt
+++ app/src/main/java/com/abadzheva/broadcast2/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.abadzheva.broadcast2
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
+import android.widget.ProgressBar
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var progressBar: ProgressBar
+
+    private val receiver =
+        object : BroadcastReceiver() {
+            override fun onReceive(
+                context: Context?,
+                intent: Intent?,
+            ) {
+                if (intent?.action == "loaded") {
+                    val percent = intent.getIntExtra("percent", 0)
+                    progressBar.progress = percent
+                }
+            }
+        }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +39,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        progressBar = findViewById<ProgressBar>(R.id.progressBar)
+
+        val intentFilter =
+            IntentFilter().apply {
+                addAction("loaded")
+            }
+        registerReceiver(receiver, intentFilter, RECEIVER_EXPORTED)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
     }
 }
